e114b266e344db7bb209b9ff8d3b37bdb7ca927b
kisraysuxi <elinatadu@icloud.com>
2023-01-27 14:13:09 +0600

    hw

--- 4hw/main.swift
+++ 4hw/main.swift
@@ -9,40 +9,29 @@ print(cars)
 
 
 //2) Создать массив из курсов валют типа Float.Создать переменную купюра с номиналом 1000 сом. Сделать так чтобы 1000 сом отобразилось в разных валютах: доллары, евро, тенге и т.д. с помощью цикла.
-var ruble = 791.154
-var dollar = 11.674
-var tenge = 5393.695
-var euro = 10.766
+var ruble = "791.154 сом"
+var dollar = "11.67 coм"
+var tenge = "5393.695 coм"
+var euro = "10.766 coм"
+//var oneThousandSom = [ruble, dollar, tenge, euro]
 var oneThousandSom = [ruble, dollar, tenge, euro]
 var som = 1000
- 
-//for soms in oneThousandSom{
-//    print("\(som)coм = \(soms)")
-//}
 
-func money(){
-    for soms in oneThousandSom{
-        if soms == 10.766{
-            print("\(som)сом = \(oneThousandSom[0]) рублей")
-            print("\(som)сом = \(oneThousandSom[1]) долларов")
-            print("\(som)сом = \(oneThousandSom[2]) тенге")
-            print("\(som)сом = \(oneThousandSom[3]) евро")
-        }
-    }
+for soms in oneThousandSom{
+    print("\(som)coм = \(soms)")
 }
-money()
 
 
 //3) Создать массив имен учитывая алфавит. Пройтись циклом и считать сколько имен имеется на букву “A” и т.
-var names = ["Alina", "Bektur", "Aliya", "Elina", "Daniel", "Artur"]
-var a = 0
-print("Ваша буква: ")
-var char_a = readLine()!
-
-for i in names{
-    if String(i.first!) == char_a{
-        a += 1
-    }
-}
-
-print("Имен на букву \(char_a) - \(a)")
+//var names = ["Alina", "Bektur", "Aliya", "Elina", "Daniel", "Artur"]
+//var a = 0
+//print("Ваша буква: ")
+//var char_a = readLine()!
+//
+//for i in names{
+//    if String(i.first!) == char_a{
+//        a += 1
+//    }
+//}
+//
+//print("Имен на букву \(char_a) - \(a)")
